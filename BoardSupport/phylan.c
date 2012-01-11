4807a57d052c516a3041cd62dd137a71568302c6
Lien Nguyen <lien.nguyen@nxp.com>
2012-01-11 11:28:22 +0700

    Fix some issues in PHY setting, Emac_EasyWeb example and VectorTableRelocation example. - Update loop condition and remove waiting for auto-negotiation done. - Update calculation of percentage value which is sent to the browser. - Update to enable debug in Flash Mode for NVIC_VectorTableRelocation example. - Update NVIC_VectorTableRelocation example to check #ifdef __RAM_MODE_ instead of #if (__RAM_MODE_ == 1)

--- BoardSupport/phylan.c
+++ BoardSupport/phylan.c
@@ -157,7 +157,7 @@ int32_t PHY_CheckStatus(uint32_t ulPHYState)
  **********************************************************************/
 int32_t PHY_SetMode(uint32_t ulPHYMode)
 {
-	int32_t id1, id2, tout, regv;
+	int32_t id1, id2, tout;
 
 	/* Check if this is a DP83848C PHY. */
 	id1 = EMAC_Read_PHY (EMAC_PHY_REG_IDR1);
