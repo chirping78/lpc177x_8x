e09e0a45acd235870d841b27b8200942af32ade4
Lien Nguyen <lien.nguyen@nxp.com>
2012-03-02 14:10:43 +0700

    Update CLKPWR: Don't disable BOD when entering to Deep Sleep Mode, Power Down Mode.

--- Drivers/source/lpc177x_8x_clkpwr.c
+++ Drivers/source/lpc177x_8x_clkpwr.c
@@ -272,7 +272,7 @@ void CLKPWR_DeepSleep(void)
 {
     /* Deep-Sleep Mode, set SLEEPDEEP bit */
 	SCB->SCR = 0x4;
-	LPC_SC->PCON = 0x8;
+	LPC_SC->PCON = 0x00;
 	/* Deep Sleep Mode*/
 	__WFI();
 }
@@ -287,7 +287,7 @@ void CLKPWR_PowerDown(void)
 {
     /* Deep-Sleep Mode, set SLEEPDEEP bit */
 	SCB->SCR = 0x4;
-	LPC_SC->PCON = 0x09;
+	LPC_SC->PCON = 0x01;
 	/* Power Down Mode*/
 	__WFI();
 }
